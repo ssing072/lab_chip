daef876c7e9ed32ac36c8b8eb3f2d51f7fe68f89
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-10-17 22:38:13 -0700

    Update main.c

--- lab_chip/source/main.c
+++ lab_chip/source/main.c
@@ -1,34 +1,39 @@
-/*	Author: ssing072
- *  Partner(s) Name: Gupta
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRA = 0x00; PORTA = 0xFF:
-    DDRB = 0xFF; PORTB = 0x00;
-    unsigned char led = 0x00;
-    unsigned char button = 0x00;
-
-    /* Insert your solution below */
-    while (1) {
-       button = ~PINA & 0x01;
-       if(button){
-           led = (led & 0xFC) | 0x01;
-       }
-       else{
-           led = (led & 0xFC) | 0x02;
-       }
-       PORTB = led;
-    }
-    return 1;
+	
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	
+	unsigned char tempA = 0x00;
+	unsigned char C = 0x00;
+	
+	while(1){
+		tempA = ~PINA;
+		C = 0x00;
+		if(tempA == 0x01 || tempA == 0x02){
+			C = 0x60;
+		}
+		else if(tempA == 0x03 || tempA == 0x04){
+			C = 0x70;
+		}
+		else if(tempA == 0x05 || tempA == 0x06){
+			C = 0x38;
+		}
+		else if(tempA == 0x07 || tempA == 0x08 || tempA == 0x09){
+			C = 0x3C;
+		}
+		else if(tempA == 0x0A || tempA == 0x0B || tempA == 0x0C){
+			C = 0x3E;
+		}
+		else if(tempA == 0x0D || tempA == 0x0E || tempA == 0x0F){
+			C = 0x3F;
+		}
+		PORTC = C;
+	}
+	return 1;
+  }
 }
