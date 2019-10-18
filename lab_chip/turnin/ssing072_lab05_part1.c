cc600c4f528f1d0f7e2be2e6e099f19c79885ad9
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-10-17 23:02:36 -0700

    Update ssing072_lab05_part1.c

--- lab_chip/turnin/ssing072_lab05_part1.c
+++ lab_chip/turnin/ssing072_lab05_part1.c
@@ -1,3 +1,12 @@
+/*	Author: ssing072
+ *  Partner(s) Name: Neha Gupta
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
